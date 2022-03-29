e2fd6e08591f6e90c7b82c749463d35f52f6656c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-03-29 13:22:19 -0300

    Hello Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-12534452c9b4bf78.d
@@ -0,0 +1,5 @@
+/Users/ferreiracode/Documents/Study/rust/the_rust_programming_language/hello_cargo/target/debug/deps/hello_cargo-12534452c9b4bf78.rmeta: src/main.rs
+
+/Users/ferreiracode/Documents/Study/rust/the_rust_programming_language/hello_cargo/target/debug/deps/hello_cargo-12534452c9b4bf78.d: src/main.rs
+
+src/main.rs:
