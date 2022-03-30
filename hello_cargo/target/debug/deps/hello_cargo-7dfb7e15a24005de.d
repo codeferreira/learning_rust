4fb647d79295dfc38ae84f59b18820e66195c229
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-03-30 19:20:28 -0300

    chore: remove target compilation files

--- hello_cargo/target/debug/deps/hello_cargo-7dfb7e15a24005de.d
+++
@@ -1,5 +0,0 @@
-/Users/ferreiracode/Documents/Study/rust/the_rust_programming_language/hello_cargo/target/debug/deps/hello_cargo-7dfb7e15a24005de.rmeta: src/main.rs
-
-/Users/ferreiracode/Documents/Study/rust/the_rust_programming_language/hello_cargo/target/debug/deps/hello_cargo-7dfb7e15a24005de.d: src/main.rs
-
-src/main.rs:
