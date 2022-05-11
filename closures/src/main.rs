d96bcd4b7cc409a8477720f369b218eb25d552a2
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-05-11 19:34:48 -0300

    refactor: remove old expensive calculation function

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -8,12 +8,6 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("Calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
     let expensive_closure = |num| {
         println!("Calculating...");
