483a186f7f811e70486aa616da36161a8868c9e2
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-05-17 16:44:16 -0300

    feat: add cacher implementation for closure

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -8,8 +8,39 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let expensive_closure = |num: u32| -> u32 {
         println!("Calculating...");
         thread::sleep(Duration::from_secs(2));
         num
